12ab829830d65fc3ea498850fc4401533b2883e2
MikolajP13 <mikolajp1313@gmail.com>
2024-08-06 19:30:49 +0200

    Created components, modules, routing, added app header

---
+++ q
@@ -0,0 +1,24 @@
+[33mcommit 31505b1a1061e4f67da56d7fd114a9f32455cb5b[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Merge: 8f15045 a22ee55
+Author: MikolajP13 <mikolajp1313@gmail.com>
+Date:   Tue Aug 6 16:20:50 2024 +0200
+
+    Merge branch 'main' of https://github.com/MikolajP13/client-register
+
+[33mcommit 8f15045aa959889b56cbd37540ffca685c4349aa[m
+Author: MikolajP13 <mikolajp1313@gmail.com>
+Date:   Tue Aug 6 16:05:02 2024 +0200
+
+    Initial commit, add bootstrap, json-server, lint and environments
+
+[33mcommit a22ee5597d1187d5d5e95b8316c162853f04c570[m
+Author: MikolajP13 <mikolajp1313@gmail.com>
+Date:   Tue Aug 6 16:05:02 2024 +0200
+
+    Initial commit
+
+[33mcommit 433504299045763b9d653952966deab44dd0e3b6[m
+Author: MikolajP13 <mikolajp1313@gmail.com>
+Date:   Mon Aug 5 21:23:16 2024 +0200
+
+    initial commit
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './modules/core/core.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeModule } from './modules/home/home.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ClientsModule } from './modules/clients/clients.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule, HomeModule, AuthModule, ClientsModule, AppRoutingModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
